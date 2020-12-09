a9bd26f3bd35698ac5aab41f66f476523ee23dc7
Cole Galloway <cole.j.galloway@gmail.com>
2020-12-08 20:02:18 -0700

    no message

--- src/gameui/SolarDefenderGUI.java
+++ src/gameui/SolarDefenderGUI.java
@@ -46,87 +46,70 @@ import gameobjects.Weapon;
 public class SolarDefenderGUI extends JFrame
 {
 
-	private Game game = new Game();
-	private CardLayout layout = new CardLayout();
+	// Game Objects
+		private Game game = new Game();
+		String[] weapons = {"No Weapon", "Phalanx", "Missile", "Rail Gun"};
+		private final int maxPlanets = 6;
+		private final String[] planetIcons = {
+			".\\Graphics\\planet1.png",
+			".\\Graphics\\planet2.png",
+			".\\Graphics\\planet3.png",
+			".\\Graphics\\planet4.png",
+			".\\Graphics\\planet5.png",
+			".\\Graphics\\planet6.png"
+		};
+		private final String[] planetNames = {
+			"Uleturn",
+			"Zahonus",
+			"Atis",
+			"Hipra",
+			"Galea",
+			"Voneon"
+		};
+		private Planet[] planets = new Planet[maxPlanets];
+		private WeaponSelector[] planetUI = new WeaponSelector[maxPlanets];
 
-	//Panels
-	private Image spaceBG = new ImageIcon(".\\Graphics\\space_background.jpg").getImage();
-	private Image spaceBG2 = new ImageIcon(".\\Graphics\\solar_system_background.jpg").getImage();
-	private JPanel mainScreen = new BackgroundImage(spaceBG);
-	private JPanel difficultyScreen = new BackgroundImage(spaceBG);
-	private	JPanel planetSelectScreen = new BackgroundImage(spaceBG);
-	private JLayeredPane gameScreen = new JLayeredPane();
-	private JPanel highScoreScreen = new BackgroundImage(spaceBG);
+		// Panels
+		private CardLayout layout = new CardLayout();
+		private Image spaceBG = new ImageIcon(".\\Graphics\\space_background.jpg").getImage();
+		private JPanel mainScreen = new BackgroundImage(spaceBG);
+		private JPanel difficultyScreen = new BackgroundImage(spaceBG);
+		private	JPanel planetSelectScreen = new BackgroundImage(spaceBG);
+		private JLayeredPane gameScreen = new JLayeredPane();
+		private JPanel highScoreScreen = new BackgroundImage(spaceBG);
 
-	//Main Screen
-	ImageIcon titleIcon = new ImageIcon(".\\Graphics\\solar_defender_title.gif");
-	private JLabel	titleGIF = new JLabel(titleIcon);
-	private JButton sButton = new JButton("Start Game");
-	private JButton hsButton = new JButton("High Scores");
-	private JButton eButton = new JButton("Exit");
+		// Main Screen
+		ImageIcon titleIcon = new ImageIcon(".\\Graphics\\solar_defender_title.gif");
+		private JLabel	titleGIF = new JLabel(titleIcon);
+		private JButton sButton = new JButton("Start Game");
+		private JButton hsButton = new JButton("High Scores");
+		private JButton eButton = new JButton("Exit");
 
-	//High Scores Screen
-	private JButton hsToMain = new JButton("< Back");
+		// High Scores Screen
+		private JButton hsToMain = new JButton("< Back");
 
-	//Difficulty Screen
-	private JButton easyButton = new JButton("Easy");
-	private JButton medButton = new JButton("Medium");
-	private JButton hardButton = new JButton("Hard");
+		// Difficulty Screen
+		private JButton easyButton = new JButton("Easy");
+		private JButton medButton = new JButton("Medium");
+		private JButton hardButton = new JButton("Hard");
+		private JButton mainMenuButton = new JButton("Main Menu");
 
-	//Planet Selection
-	String[] weapons = {"No Weapon", "Phalanx", "Missile", "RailGun"};
-	boolean check1 = false, check2 = false, check3 = false, check4 = false, check5 = false, check6 = false;
-	private JRadioButton planet1Radio = new JRadioButton("Uleturn");
-	private JRadioButton planet2Radio = new JRadioButton("Zahonus");
-	private JRadioButton planet3Radio = new JRadioButton("Atis");
-	private JRadioButton planet4Radio = new JRadioButton("Hipra");
-	private JRadioButton planet5Radio = new JRadioButton("Galea");
-	private JRadioButton planet6Radio = new JRadioButton("Voneon");
-	private JComboBox<String> planet1WeaponSelect = new JComboBox<String>(weapons);
-	private JComboBox<String> planet2WeaponSelect = new JComboBox<String>(weapons);
-	private JComboBox<String> planet3WeaponSelect = new JComboBox<String>(weapons);
-	private JComboBox<String> planet4WeaponSelect = new JComboBox<String>(weapons);
-	private JComboBox<String> planet5WeaponSelect = new JComboBox<String>(weapons);
-	private JComboBox<String> planet6WeaponSelect = new JComboBox<String>(weapons);
-	private	JButton planetToDifficulty = new JButton("< Back");
-	private	JButton planetConfirm = new JButton("Confirm");
+		// Planet Selection
+		private	JButton planetToDifficulty = new JButton("< Back");
+		private	JButton planetConfirm = new JButton("Confirm");
 
-	//Game Screen
-	private JButton pauseMenuButton = new JButton("Menu");
-	static JButton viewScores = new JButton("Did you get a high Score?");
-	private GamePanel gamePanel = new GamePanel(this.game, spaceBG);
-
-	//General Buttons
-	private JButton mainMenuButton = new JButton("Main Menu");
-
-	//Game Objects
-	ImageIcon planet1Icon = new ImageIcon(".\\Graphics\\planet1.png");
-	ImageIcon planet2Icon = new ImageIcon(".\\Graphics\\planet2.png");
-	ImageIcon planet3Icon = new ImageIcon(".\\Graphics\\planet3.png");
-	ImageIcon planet4Icon = new ImageIcon(".\\Graphics\\planet4.png");
-	ImageIcon planet5Icon = new ImageIcon(".\\Graphics\\planet5.png");
-	ImageIcon planet6Icon = new ImageIcon(".\\Graphics\\planet6.png");
-	private JLabel planet1Label = new JLabel(planet1Icon);
-	private JLabel planet2Label = new JLabel(planet2Icon);
-	private JLabel planet3Label = new JLabel(planet3Icon);
-	private JLabel planet4Label = new JLabel(planet4Icon);
-	private JLabel planet5Label = new JLabel(planet5Icon);
-	private JLabel planet6Label = new JLabel(planet6Icon);
-	Planet planet1 = new Planet(0.3 + 0.5 * (0.0 / 5.0));
-	Planet planet2 = new Planet(0.3 + 0.5 * (1.0 / 5.0));
-	Planet planet3 = new Planet(0.3 + 0.5 * (2.0 / 5.0));
-	Planet planet4 = new Planet(0.3 + 0.5 * (3.0 / 5.0));
-	Planet planet5 = new Planet(0.3 + 0.5 * (4.0 / 5.0));
-	Planet planet6 = new Planet(0.3 + 0.5 * (5.0 / 5.0));
-	Weapon weapon1 = null;
+		// Game Screen
+		private JButton pauseMenuButton = new JButton("Menu");
+		private GamePanel gamePanel = new GamePanel(this.game, spaceBG);
+	
 	
 	private HighScores highScore = new HighScores();
-
+	static JButton viewScores = new JButton("Did you get a high Score?");
+	
 	public
 	SolarDefenderGUI ()
 	{
 		super("Solar Defender");
-		//this.setMinimumSize(new Dimension(320, 240));
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLayout(layout);
 		this.add(mainScreen, "MAIN");
@@ -135,15 +118,12 @@ public class SolarDefenderGUI extends JFrame
 		this.add(gameScreen, "GAME");
 		this.add(highScoreScreen, "HIGHSCORES");
 
-		planet1.spriteIdx = 1;
-		planet2.spriteIdx = 2;
-		planet3.spriteIdx = 3;
-		planet4.spriteIdx = 4;
-		planet5.spriteIdx = 5;
-		planet6.spriteIdx = 6;
-		
-		HighScores.saveData(highScore);
-		
+		for (int i = 0; i < maxPlanets; i++)
+		{
+			planets[i] = new Planet(0.3 + 0.5 * ((double)i / (double)(maxPlanets - 1)));
+			planets[i].spriteIdx = i + 1;
+		}
+
 		buildMainScreen();
 		buildDiffScreen();
 		buildPlanetSelectScreen();
@@ -159,8 +139,6 @@ public class SolarDefenderGUI extends JFrame
 		System.out.printf("main pane H=%d, W=%d\n", this.getContentPane().getHeight(), this.getContentPane().getWidth());
 		System.out.printf("difficultyScreen H=%d, W=%d\n", difficultyScreen.getHeight(), difficultyScreen.getWidth());
 		System.out.printf("mainScreen H=%d, W=%d\n", mainScreen.getHeight(), mainScreen.getWidth());
-		
-		
 	}
 
 	private void
@@ -173,6 +151,7 @@ public class SolarDefenderGUI extends JFrame
 		content.setOpaque(false);
 		mainScreen.add(content);
 
+		titleGIF.setOpaque(false);
 		sButton.addActionListener(new ButtonListener());
 		hsButton.addActionListener(new ButtonListener());
 		eButton.addActionListener(new ButtonListener());
@@ -196,7 +175,6 @@ public class SolarDefenderGUI extends JFrame
 				.addGap(50)
 				.addComponent(eButton, GroupLayout.DEFAULT_SIZE, 25, 50)
 				);
-		
 	}
 
 	private void
@@ -302,87 +280,42 @@ public class SolarDefenderGUI extends JFrame
 	private void
 	buildPlanetSelectScreen ()
 	{
-		planetSelectScreen.setLayout(null);
-
-		planet1Label.setBounds(50, 50, 100, 100);
-		planet2Label.setBounds(50, 150, 100, 100);
-		planet3Label.setBounds(50, 250, 100, 100);
-		planet4Label.setBounds(50, 350, 100, 100);
-		planet5Label.setBounds(50, 450, 100, 100);
-		planet6Label.setBounds(50, 550, 100, 100);
-
-		planet1Radio.setBounds(150, 90, 100, 20);
-		planet2Radio.setBounds(150, 190, 100, 20);
-		planet3Radio.setBounds(150, 290, 100, 20);
-		planet4Radio.setBounds(150, 390, 100, 20);
-		planet5Radio.setBounds(150, 490, 100, 20);
-		planet6Radio.setBounds(150, 590, 100, 20);
-
-		planet1Radio.setForeground(Color.WHITE);
-		planet2Radio.setForeground(Color.WHITE);
-		planet3Radio.setForeground(Color.WHITE);
-		planet4Radio.setForeground(Color.WHITE);
-		planet5Radio.setForeground(Color.WHITE);
-		planet6Radio.setForeground(Color.WHITE);
-
-		planet1Radio.setBackground(Color.BLACK);
-		planet2Radio.setBackground(Color.BLACK);
-		planet3Radio.setBackground(Color.BLACK);
-		planet4Radio.setBackground(Color.BLACK);
-		planet5Radio.setBackground(Color.BLACK);
-		planet6Radio.setBackground(Color.BLACK);
-
-		planet1WeaponSelect.setBounds(250, 90, 100, 20);
-		planet2WeaponSelect.setBounds(250, 190, 100, 20);
-		planet3WeaponSelect.setBounds(250, 290, 100, 20);
-		planet4WeaponSelect.setBounds(250, 390, 100, 20);
-		planet5WeaponSelect.setBounds(250, 490, 100, 20);
-		planet6WeaponSelect.setBounds(250, 590, 100, 20);
-
-		planetToDifficulty.setBounds(350, 850, 125, 25);
-		planetConfirm.setBounds(500, 850, 125, 25);
-
-		planet1Radio.addActionListener(new ButtonListener()); 
-		planet2Radio.addActionListener(new ButtonListener());
-		planet3Radio.addActionListener(new ButtonListener());
-		planet4Radio.addActionListener(new ButtonListener());
-		planet5Radio.addActionListener(new ButtonListener());
-		planet6Radio.addActionListener(new ButtonListener());
-
-		planet1WeaponSelect.addActionListener(new ButtonListener());
-		planet2WeaponSelect.addActionListener(new ButtonListener());
-		planet3WeaponSelect.addActionListener(new ButtonListener());
-		planet4WeaponSelect.addActionListener(new ButtonListener());
-		planet5WeaponSelect.addActionListener(new ButtonListener());
-		planet6WeaponSelect.addActionListener(new ButtonListener());
+		planetSelectScreen.setLayout(new GridBagLayout());
+		JPanel content = new JPanel();
+		GroupLayout layout = new GroupLayout(content);
+		content.setLayout(layout);
+		content.setOpaque(false);
+		planetSelectScreen.add(content);
 
 		planetToDifficulty.addActionListener(new ButtonListener());
 		planetConfirm.addActionListener(new ButtonListener());
 
-		planetSelectScreen.add(planet1Label);
-		planetSelectScreen.add(planet2Label);
-		planetSelectScreen.add(planet3Label);
-		planetSelectScreen.add(planet4Label);
-		planetSelectScreen.add(planet5Label);
-		planetSelectScreen.add(planet6Label);
+		Group horGroup = layout.createParallelGroup(GroupLayout.Alignment.CENTER);
+		Group verGroup = layout.createSequentialGroup();
 
-		planetSelectScreen.add(planet1Radio);
-		planetSelectScreen.add(planet2Radio);
-		planetSelectScreen.add(planet3Radio);
-		planetSelectScreen.add(planet4Radio);
-		planetSelectScreen.add(planet5Radio);
-		planetSelectScreen.add(planet6Radio);
+		for (int i = 0; i < maxPlanets; i++)
+		{
+			WeaponSelector ui = new WeaponSelector(planetIcons[i], planetNames[i], weapons);
+			horGroup.addComponent(ui);
+			verGroup.addComponent(ui);
+			planetUI[i] = ui;
+		}
 
-		planetSelectScreen.add(planet1WeaponSelect);
-		planetSelectScreen.add(planet2WeaponSelect);
-		planetSelectScreen.add(planet3WeaponSelect);
-		planetSelectScreen.add(planet4WeaponSelect);
-		planetSelectScreen.add(planet5WeaponSelect);
-		planetSelectScreen.add(planet6WeaponSelect);
+		horGroup.addGroup(layout.createSequentialGroup()
+			.addComponent(planetToDifficulty, GroupLayout.DEFAULT_SIZE, 125, 150)
+			.addGap(20)
+			.addComponent(planetConfirm, GroupLayout.DEFAULT_SIZE, 125, 150)
+		);
+		verGroup.addGap(30);
+		verGroup.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+			.addComponent(planetToDifficulty,  GroupLayout.DEFAULT_SIZE, 25, 50)
+			.addComponent(planetConfirm, GroupLayout.DEFAULT_SIZE, 25, 50)
+		);
 
-		planetSelectScreen.add(planetToDifficulty);
-		planetSelectScreen.add(planetConfirm);
+		layout.setHorizontalGroup(horGroup);
+		layout.setVerticalGroup(verGroup);
 	}
+
 	
 	public void 
 	Scores() {
@@ -677,129 +610,37 @@ public class SolarDefenderGUI extends JFrame
 			}
 
 		}
-		public void checkSelection() {
+		private void
+		checkSelection ()
+		{
+			for (int i = 0; i < maxPlanets; i++)
+			{
+				if (planetUI[i].isSelected())
+				{
+					game.addPlanet(planets[i]);
 
-			if (planet1Radio.isSelected()) {
-				game.addPlanet(planet1);
-
-				if(planet1WeaponSelect.getSelectedItem() == "No Weapon") {
-					planet1.setWeapon(new NoWeapon());
+					switch (planetUI[i].getSelectedWeapon())
+					{
+					case "Phalanx":
+						planets[i].setWeapon(new Phalanx());
+						break;
+					case "Missile":
+						planets[i].setWeapon(new Missile());
+						break;
+					case "Rail Gun":
+						planets[i].setWeapon(new RailGun());
+						break;
+					default:
+						System.out.printf("UNHANDLED WEAPON %s", planetUI[i].getSelectedWeapon());
+					case "No Weapon":
+						planets[i].setWeapon(new NoWeapon());
+						break;
+					}
 				}
-				else if(planet1WeaponSelect.getSelectedItem() == "Phalanx") {
-					planet1.setWeapon(new Phalanx());
+				else
+				{
+					game.removePlanet(planets[i]);
 				}
-				else if(planet1WeaponSelect.getSelectedItem() == "Missile") {
-					planet1.setWeapon(new Missile());
-				}
-				else if(planet1WeaponSelect.getSelectedItem() == "Rail Gun") {
-					planet1.setWeapon(new RailGun());
-				}
-			}
-			else {
-				game.removePlanet(planet1);
-			}
-
-			if (planet2Radio.isSelected()) {
-				game.addPlanet(planet2);
-
-				if(planet2WeaponSelect.getSelectedItem() == "No Weapon") {
-					planet2.setWeapon(new NoWeapon());
-				}
-				else if(planet2WeaponSelect.getSelectedItem() == "Phalanx") {
-					planet2.setWeapon(new Phalanx());
-				}
-				else if(planet2WeaponSelect.getSelectedItem() == "Missile") {
-					planet2.setWeapon(new Missile());
-				}
-				else if(planet2WeaponSelect.getSelectedItem() == "Rail Gun") {
-					planet2.setWeapon(new RailGun());
-				}
-			}
-			else {
-				game.removePlanet(planet2);
-			}
-
-			if (planet3Radio.isSelected()) {
-				game.addPlanet(planet3);
-
-				if(planet3WeaponSelect.getSelectedItem() == "No Weapon") {
-					planet3.setWeapon(new NoWeapon());
-				}
-				else if(planet3WeaponSelect.getSelectedItem() == "Phalanx") {
-					planet3.setWeapon(new Phalanx());
-				}
-				else if(planet3WeaponSelect.getSelectedItem() == "Missile") {
-					planet3.setWeapon(new Missile());
-				}
-				else if(planet3WeaponSelect.getSelectedItem() == "Rail Gun") {
-					planet3.setWeapon(new RailGun());
-				}
-
-			}
-			else {
-				game.removePlanet(planet3);
-			}
-
-			if (planet4Radio.isSelected()) {
-				game.addPlanet(planet4);
-
-				if(planet4WeaponSelect.getSelectedItem() == "No Weapon") {
-					planet4.setWeapon(new NoWeapon());
-				}
-				else if(planet4WeaponSelect.getSelectedItem() == "Phalanx") {
-					planet4.setWeapon(new Phalanx());
-				}
-				else if(planet4WeaponSelect.getSelectedItem() == "Missile") {
-					planet4.setWeapon(new Missile());
-				}
-				else if(planet4WeaponSelect.getSelectedItem() == "Rail Gun") {
-					planet4.setWeapon(new RailGun());
-				}
-			}
-			else {
-				game.removePlanet(planet4);
-			}
-
-			if (planet5Radio.isSelected()) {
-				game.addPlanet(planet5);
-
-				if(planet5WeaponSelect.getSelectedItem() == "No Weapon") {
-					planet5.setWeapon(new NoWeapon());
-				}
-				else if(planet5WeaponSelect.getSelectedItem() == "Phalanx") {
-					planet5.setWeapon(new Phalanx());
-				}
-				else if(planet5WeaponSelect.getSelectedItem() == "Missile") {
-					planet5.setWeapon(new Missile());
-				}
-				else if(planet5WeaponSelect.getSelectedItem() == "Rail Gun") {
-					planet5.setWeapon(new RailGun());
-				}
-			}
-			else {
-				game.removePlanet(planet5);
-			}
-
-			if (planet6Radio.isSelected()) {
-				game.addPlanet(planet6);
-
-				if(planet6WeaponSelect.getSelectedItem() == "No Weapon") {
-					planet6.setWeapon(new NoWeapon());
-				}
-				else if(planet6WeaponSelect.getSelectedItem() == "Phalanx") {
-					planet6.setWeapon(new Phalanx());
-				}
-				else if(planet6WeaponSelect.getSelectedItem() == "Missile") {
-					planet6.setWeapon(new Missile());
-				}
-				else if(planet6WeaponSelect.getSelectedItem() == "Rail Gun") {
-					planet6.setWeapon(new RailGun());
-				}
-				System.out.println(planet6WeaponSelect.getSelectedItem());
-			}
-			else {
-				game.removePlanet(planet6);
-			}
 		}
 	}
 
